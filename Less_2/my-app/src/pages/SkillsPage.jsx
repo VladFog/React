26d9ba49cadb1a6d883377e340cdd8073a472a4a
VladFog <vgulida341@gmail.com>
2022-12-21 20:18:42 +0200

    Less_2

---
+++ Less_2/my-app/src/pages/SkillsPage.jsx
@@ -0,0 +1,28 @@
+const SkillsPage = () => {
+  return (
+    <>
+      <div style={{ height: '77.6vh' }} className="page">
+        <div>
+          <div className="aboutme__container _container">
+            <p>my skills</p>
+            <br />
+            <br />
+            <div className="about__subtitle">
+              Dolor sit amet, consectetur adipiscing elit viverra tristique <br /> placerat in massa
+              consectetur quisque
+            </div>
+            <div className="about__text">
+              Faucibus sed tristique fames sed aliquet ultricies eget viverra arcu. Vitae faucibus
+              diam consequat maecenas. Turpis metus sit diam purus leo in varius ac quam. Nunc amet
+              tristique volutpat adipiscing vulputate phasellus. Volutpat faucibus praesent ac
+              lobortis aliquam dolor.
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SkillsPage;
